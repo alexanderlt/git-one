4f30fd4db5e3e5684f0cff1768c86621110a3205
Alex <alexanderqd@hotmail.com>
2018-05-18 00:12:46 +0800

    2018-5-18 test

--- feature-b.js
+++ feature-b.js
@@ -1,4 +1,6 @@
 console.log('feature-b');
 console.log('local add');
+console.log('add at 2018-5-18 00:16');
+console.log('local add at 2018-5-18 00:12');
 console.log('add at 2018-5-18 00:06');
 console.log('origin add at 2018-5-18 00:11');
